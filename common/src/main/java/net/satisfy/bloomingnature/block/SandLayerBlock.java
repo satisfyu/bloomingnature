1c6148e02647696b0ce78521d5520803c48877f3
berksire <laube_marco@yahoo.de>
2024-07-14 17:14:53 +0200

    Fixed: Savanna Plateau Grass, Sandlayer, Oasis Structure

--- common/src/main/java/net/satisfy/bloomingnature/block/SandLayerBlock.java
+++ common/src/main/java/net/satisfy/bloomingnature/block/SandLayerBlock.java
@@ -1,17 +1,110 @@
 package net.satisfy.bloomingnature.block;
 
-import net.minecraft.world.level.block.SnowLayerBlock;
-import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.LevelReader;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.LightLayer;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.IntegerProperty;
+import net.minecraft.world.level.pathfinder.PathComputationType;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-public class SandLayerBlock extends SnowLayerBlock {
-    public SandLayerBlock(Properties properties) {
+@SuppressWarnings("deprecation")
+public class SandLayerBlock extends Block {
+    public static final IntegerProperty LAYERS;
+    protected static final VoxelShape[] SHAPE_BY_LAYER;
+
+    public SandLayerBlock(BlockBehaviour.Properties properties) {
         super(properties);
+        this.registerDefaultState(this.stateDefinition.any().setValue(LAYERS, 1));
     }
 
-    @Override
-    public boolean canSurvive(BlockState state, LevelReader world, BlockPos pos) {
+    public boolean isPathfindable(BlockState blockState, BlockGetter blockGetter, BlockPos blockPos, PathComputationType pathComputationType) {
+        return switch (pathComputationType) {
+            case LAND -> blockState.getValue(LAYERS) < 5;
+            case WATER, AIR -> false;
+        };
+    }
+
+    public @NotNull VoxelShape getShape(BlockState blockState, BlockGetter blockGetter, BlockPos blockPos, CollisionContext collisionContext) {
+        return SHAPE_BY_LAYER[blockState.getValue(LAYERS)];
+    }
+
+    public @NotNull VoxelShape getCollisionShape(BlockState blockState, BlockGetter blockGetter, BlockPos blockPos, CollisionContext collisionContext) {
+        return SHAPE_BY_LAYER[blockState.getValue(LAYERS) - 1];
+    }
+
+    public @NotNull VoxelShape getBlockSupportShape(BlockState blockState, BlockGetter blockGetter, BlockPos blockPos) {
+        return SHAPE_BY_LAYER[blockState.getValue(LAYERS)];
+    }
+
+    public @NotNull VoxelShape getVisualShape(BlockState blockState, BlockGetter blockGetter, BlockPos blockPos, CollisionContext collisionContext) {
+        return SHAPE_BY_LAYER[blockState.getValue(LAYERS)];
+    }
+
+    public boolean useShapeForLightOcclusion(BlockState blockState) {
         return true;
+    }
+
+    public float getShadeBrightness(BlockState blockState, BlockGetter blockGetter, BlockPos blockPos) {
+        return blockState.getValue(LAYERS) == 8 ? 0.2F : 1.0F;
+    }
+
+    public @NotNull BlockState updateShape(BlockState blockState, Direction direction, BlockState blockState2, LevelAccessor levelAccessor, BlockPos blockPos, BlockPos blockPos2) {
+        return !blockState.canSurvive(levelAccessor, blockPos) ? Blocks.AIR.defaultBlockState() : super.updateShape(blockState, direction, blockState2, levelAccessor, blockPos, blockPos2);
+    }
+
+    public void randomTick(BlockState blockState, ServerLevel serverLevel, BlockPos blockPos, RandomSource randomSource) {
+        if (serverLevel.getBrightness(LightLayer.BLOCK, blockPos) > 11) {
+            dropResources(blockState, serverLevel, blockPos);
+            serverLevel.removeBlock(blockPos, false);
+        }
+
+    }
+
+    public boolean canBeReplaced(BlockState blockState, BlockPlaceContext blockPlaceContext) {
+        int i = blockState.getValue(LAYERS);
+        if (blockPlaceContext.getItemInHand().is(this.asItem()) && i < 8) {
+            if (blockPlaceContext.replacingClickedOnBlock()) {
+                return blockPlaceContext.getClickedFace() == Direction.UP;
+            } else {
+                return true;
+            }
+        } else {
+            return i == 1;
+        }
+    }
+
+    @Nullable
+    public BlockState getStateForPlacement(BlockPlaceContext blockPlaceContext) {
+        BlockState blockState = blockPlaceContext.getLevel().getBlockState(blockPlaceContext.getClickedPos());
+        if (blockState.is(this)) {
+            int i = blockState.getValue(LAYERS);
+            return blockState.setValue(LAYERS, Math.min(8, i + 1));
+        } else {
+            return super.getStateForPlacement(blockPlaceContext);
+        }
+    }
+
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(LAYERS);
+    }
+
+    static {
+        LAYERS = BlockStateProperties.LAYERS;
+        SHAPE_BY_LAYER = new VoxelShape[]{Shapes.empty(), Block.box(0.0, 0.0, 0.0, 16.0, 2.0, 16.0), Block.box(0.0, 0.0, 0.0, 16.0, 4.0, 16.0), Block.box(0.0, 0.0, 0.0, 16.0, 6.0, 16.0), Block.box(0.0, 0.0, 0.0, 16.0, 8.0, 16.0), Block.box(0.0, 0.0, 0.0, 16.0, 10.0, 16.0), Block.box(0.0, 0.0, 0.0, 16.0, 12.0, 16.0), Block.box(0.0, 0.0, 0.0, 16.0, 14.0, 16.0), Block.box(0.0, 0.0, 0.0, 16.0, 16.0, 16.0)};
     }
 }
